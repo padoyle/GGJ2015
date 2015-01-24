bb07f891aa857717557c1a67f47a4ce77f6f2a3f
jaldag <jamesaldag@gmail.com>
2015-01-23 22:11:51 -0800

    Characters are now part of background. Start and Initial screens added

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ var labelHeight = 100;
 
 var text;
 
+var background;
+
 var option1;
 var option2;
 var option3;
@@ -25,30 +27,75 @@ var hat3;
 var hat4;
 
 //Base Classes
-var Character = Class.create(Sprite, {
-	initialize: function(x, y, width, height, model, hat) {
+
+var Hat = Class.create(Sprite, {
+	initialize: function(x, y, width, height, model) {
 		Sprite.call(this, width, height);
 		this.x = x;
 		this.y = y;
-		this.image = game.assets[model];
-		this.hat = hat;
-		hat.x = ((this.x * 2 + this.width) / 2) - (hat.width / 2);
-		hat.y = this.y - hat.height + 1;
-		game.rootScene.addChild(hat);
-	}
-});
-
-var Hat = Class.create(Sprite, {
-	initialize: function(width, height, model) {
-		Sprite.call(this, width, height);
 		this.image = game.assets[model];
 	}
 });
 
 var Background = Class.create(Sprite, {
-	initialize: function(imageName) {
+	initialize: function(imageName, hat1, hat2, hat3, hat4) {
 		Sprite.call(this, gameWidth, gameHeight - labelHeight);
 		this.image = game.assets[imageName];
+		
+		game.rootScene.addChild(this);
+		game.rootScene.addChild(hat1);
+		game.rootScene.addChild(hat2);
+		game.rootScene.addChild(hat3);
+		game.rootScene.addChild(hat4);
+	}
+});
+
+var Start = new Class(Sprite, {
+	initialize: function() {
+		Sprite.call(this, gameWidth, gameHeight - labelHeight);
+		this.image = game.assets['images/startScreen.png'];
+	},
+	onenterframe: function() {
+		if (game.input['Enter']) {
+			game.rootScene.removeChild(this);
+			game.rootScene.addChild(new Initial());
+		}
+	}
+});
+
+var initNewScenario = function() {
+	//Eventually, we'll pull a random background
+	hat1 = new Hat(150, 150, 15, 15, "images/hat.png");
+	hat2 = new Hat(200, 200, 15, 15, "images/hat.png");
+	hat3 = new Hat(250, 250, 15, 15, "images/hat.png");
+	hat4 = new Hat(300, 300, 15, 15, "images/hat.png");
+	background = new Background("images/bg.png", hat1, hat2, hat3, hat4);
+	
+			
+	option1 = new Option1();
+	option2 = new Option2();
+	option3 = new Option3();
+	option4 = new Option4();
+	
+	game.rootScene.addChild(option1);
+	game.rootScene.addChild(option2);
+	game.rootScene.addChild(option3);
+	game.rootScene.addChild(option4);
+		
+	text = new ButtonText();
+	game.rootScene.addChild(text);
+};
+
+var Initial = new Class(Sprite, {
+	initialize: function() {
+		Sprite.call(this, gameWidth, gameHeight - labelHeight);
+		this.image = game.assets['images/initialScreen.png'];
+	},
+	onenterframe: function() {
+		if (game.input['Enter']) {
+			game.rootScene.removeChild(this);
+			initNewScenario();
+		}
 	}
 });
 
@@ -66,7 +113,7 @@ var Option1 = Class.create(Label, {
 		Label.call(this);
 		this.x = 400;
 		this.y = gameHeight - labelHeight + 1;
-		this.text = "Option 3 here " + character1.x + " " + character1.y;
+		this.text = "Option 3 here " + hat1.x + " " + hat1.y;
 	}
 });
 
@@ -75,7 +122,7 @@ var Option2 = Class.create(Label, {
 		Label.call(this);
 		this.x = 700;
 		this.y = gameHeight - labelHeight + 1;
-		this.text = "Option 3 here " + character2.x + " " + character2.y;
+		this.text = "Option 3 here " + hat2.x + " " + hat2.y;
 	}
 });
 
@@ -84,7 +131,7 @@ var Option3 = Class.create(Label, {
 		Label.call(this);
 		this.x = 400;
 		this.y = gameHeight - (labelHeight / 2) + 1;
-		this.text = "Option 3 here " + character3.x + " " + character3.y;
+		this.text = "Option 3 here " + hat3.x + " " + hat3.y;
 	}
 });
 
@@ -93,51 +140,25 @@ var Option4 = Class.create(Label, {
 		Label.call(this);
 		this.x = 700;
 		this.y = gameHeight - (labelHeight / 2) + 1;
-		this.text = "Option 3 here " + character4.x + " " + character4.y;
+		this.text = "Option 3 here " + hat4.x + " " + hat4.y;
 	}
 });
 
 // When document loads, set up basic game
 window.onload = function() {
 	game = new Game(gameWidth, gameHeight);
-	game.preload('images/bg.png', 'images/hat.png', 'images/dude.png');
+	game.preload('images/bg.png', 'images/hat.png',
+	              'images/startScreen.png', 'images/initialScreen.png');
 	
 	game.fps = 60;
 	game.scale = 1;
+	
+	//Keybindings for the game
+	game.keybind(13, 'Enter');
 
 	game.onload = function() {
-		var bg = new Background("images/bg.png");
-		game.rootScene.addChild(bg);
-		
-		hat1 = new Hat(15, 15, "images/hat.png");
-		hat2 = new Hat(15, 15, "images/hat.png");
-		hat3 = new Hat(15, 15, "images/hat.png");
-		hat4 = new Hat(15, 15, "images/hat.png");
-		
-		character1 = new Character(200, 200, 16, 28, "images/dude.png", hat1);
-		game.rootScene.addChild(character1);
-		
-		character2 = new Character(250, 250, 16, 28, "images/dude.png", hat2);
-		game.rootScene.addChild(character2);
-		
-		character3 = new Character(300, 300, 16, 28, "images/dude.png", hat3);
-		game.rootScene.addChild(character3);
-		
-		character4 = new Character(350, 350, 16, 28, "images/dude.png", hat4);
-		game.rootScene.addChild(character4);
-		
-		text = new ButtonText();
-		game.rootScene.addChild(text);
-		
-		option1 = new Option1();
-		option2 = new Option2();
-		option3 = new Option3();
-		option4 = new Option4();
-		
-		game.rootScene.addChild(option1);
-		game.rootScene.addChild(option2);
-		game.rootScene.addChild(option3);
-		game.rootScene.addChild(option4);
+		start = new Start();
+		game.rootScene.addChild(start);
 	};
     game.start();
 };
